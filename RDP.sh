125d4eb59118e32d3ad0620e33fea94cf57e3375
AAron <84721533+aarongecai@users.noreply.github.com>
2021-05-24 22:39:27 +0800

    Update RDP.sh
    change user to AGID

--- RDP.sh
+++ RDP.sh
@@ -2,8 +2,9 @@
 printf "Installing RDP Be Patience... " >&2
 {
 sudo useradd -m FSID
-sudo adduser FSID sudo
-echo 'FSID:8426' | sudo chpasswd
+sudo useradd -m AGID
+sudo adduser AGID sudo
+echo 'AGID:1234' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -18,17 +19,17 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser FSID chrome-remote-desktop
+sudo adduser AGID chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - FSID -c """$CRP"""
+su - AGID -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
-    printf "\n\nUpgrade Completed " >&2
+    printf "\n\nUpgrade Completed." >&2
 else
-    printf "\n\nError Occured " >&2
+    printf "\n\nError Occured, Please check." >&2
 fi
